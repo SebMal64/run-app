d02d01cfb800500e6063ef9e72e79b462d2a3b0d
Sebastian Malasquez <sebasmalasquez@gmail.com>
2023-07-14 22:02:36 -0500

    segundo

---
+++ src/Actual.js
@@ -0,0 +1,12 @@
+
+import './Actual.css'
+
+
+function Actual() {
+    return (
+        <div className="actual">
+            <h1 className='h1-actual'>Run App</h1>
+        </div>
+    )
+}
+export default Actual
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Actual from './Actual';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Actual/>
+  </React.StrictMode>
+);
+
