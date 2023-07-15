be2f412a2b4e3a716bc6bdc4176ef3a9cc311c46
Sebastian Malasquez <sebasmalasquez@gmail.com>
2023-07-14 22:25:44 -0500

    tercero

--- src/Actual.js
+++ src/Actual.js
@@ -7,6 +7,6 @@ function Actual() {
         <div className="actual">
             <h1 className='h1-actual'>Run Apper</h1>
         </div>
-    )
+    );
 }
 export default Actual
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import Actual from './Actual';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
     <Actual/>
   </React.StrictMode>
 );
