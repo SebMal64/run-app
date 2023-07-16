0ca7b8e8e2fde431e4686fc507250806df90e517
Sebastian Malasquez <sebasmalasquez@gmail.com>
2023-07-15 23:23:48 -0500

    Portada

--- src/Actual.js
+++ src/Actual.js
@@ -4,9 +4,15 @@ import './Actual.css'
 
 function Actual() {
     return (
+        <div className="cont">
         <div className="actual">
-            <h1 className="h1-actual">Run Apper</h1>
-        </div>
+            <h1 className="h1-actual">Run App</h1>
+            <div className="cont-btn">
+                <button className="btn-main">Empezar</button>
+                <button className="btn-main">Registrarme</button>
+
+            </div>
+        </div></div>
     );
 }
 export default Actual
