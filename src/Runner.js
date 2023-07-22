a396e5cb2e6e8f44c89954f888ec46871b96ecab
Sebastian Malasquez <sebasmalasquez@gmail.com>
2023-07-21 22:38:21 -0500

    mensaje

---
+++ src/Runner.js
@@ -0,0 +1,28 @@
+import './Runner.css';
+import img1 from './assets/img/image1.png';
+import img2 from './assets/img/image2.png';
+
+function Runner() {
+    return (
+        <div className='cont-2'>
+            <div className='p-1'>
+                <h1 class="h1-edit">run app</h1>
+                <div className='grid-main'>
+                    <div className='box time'>
+                        <h3 className="h3-edit">Tiempo</h3>
+                        <h2 className="tiempo">25:50</h2>
+                    </div>
+                    <div className='box image1'><img class="img-edit" alt="" src={img1} />
+                    <h3 className="h3-edit">Correr</h3></div>
+                    <div className='box image2'><img class="img-edit" alt=""  src={img2} />
+                    <h3 className="h3-edit">Caminar</h3></div>
+                </div>
+                <div className='btn-grid'>
+                <button className="btn-run b1">Continuar</button>
+                <button className="btn-run b2">Terminar</button>
+                </div>
+        </div></div>
+    )
+};
+
+export default Runner;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import Actual from './Actual';
 import './Actual.css';
+import Runner from './Runner';
+import './Runner.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Actual/>
+    <Runner/>
   </React.StrictMode>
 );
 
